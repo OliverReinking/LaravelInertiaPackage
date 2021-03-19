7741b9b59403aaa692e91fb4ea6ba18eccf7ecf9
Oliver Reinking <oliverreinking@hotmail.com>
2021-03-19 17:42:17 +0100

    Fehlerkorrekturen durchgeführt

---
+++ resources/lang/de/auth.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    'Login' => 'Anmeldung',
+    'Name' => 'Name',
+    'E-Mail Address' => 'Mailadresse',
+    'Password' => 'Kennwort',
+    'OldPassword' => 'Altes Kennwort',
+    'NewPassword' => 'Neues Kennwort',
+    'Confirm Password' => 'Kennwort bestätigen',
+
+    'Current Password' => 'Das alte Kennwort stimmt nicht mit dem aktuellen Kennwort überein. Versuchen Sie es bitte erneut.',
+    'New Password' => 'Das neue Kennwort darf nicht mit dem bisherigen Kennwort übereinstimmen. Bitte wählen Sie ein anderes Kennwort aus.',
+    'Password changed' => 'Das Kennwort wurde erfolgreich geändert.',
+
+    'Remember Me' => 'Erinnere Dich an mich',
+    'Forgot Your Password?' => 'Kennwort vergessen?',
+    'Request a password' => 'Kennwort jetzt anfordern',
+    'Don\'t have an account?' => 'Sie haben sich noch nicht registriert?',
+    'Register' => 'Jetzt registrieren',
+    'Already have an account?' => 'Sie sind bereits registriert?',
+    'A fresh verification link has been sent to your email address.' => 'Soeben wurde ein Authentifizierungslink an Ihre Mailadresse versendet.',
+    'Verify Your Email Address' => 'Verifizieren Sie bitte Ihre Mailadresse.',
+    'Before proceeding, please check your email for a verification link.' => 'Bevor Sie fortfahren, bitte überprüfen Sie ihren Maileingang.',
+    'send verification email' => 'Sende Mail mit Authentifizierungslink',
+    'Reset Password' => 'Kennwort zurücksetzen',
+    'Send Password Reset Link' => 'Authentifizierungslink versenden',
+    'Back to login' => 'Zurück zur Anmeldung',
+
+    'failed' => 'Die Mailadresse und das Kennwort sind unbekannt.',
+    'throttle' => 'Zu viele Login-Versuche. Bitte versuchen Sie es in :seconds Sekunden erneut.',
+
+];
---
+++ resources/lang/de/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Das Kennwort muss aus mindestens acht Zeichen bestehen und mit der Kennwortbestätigung übereinstimmen.',
+    'reset' => 'Ihr Kennwort wurde zurückgesetzt.',
+    'sent' => 'Wir haben Ihnen eine Mail mit einem Authentifizierungslink zugesendet!',
+    'token' => 'Das Kennworttoken ist ungültig.',
+    'user' => "Die vorgegebene Mailadresse konnten wir leider nicht finden.",
+
+];
